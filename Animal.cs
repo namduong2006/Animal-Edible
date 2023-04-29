1d225ed29d912b5a0626a33d1c62f6aabdb6147b
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-29 19:55:00 +0700

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Edible
+{
+    public abstract class Animal
+    {
+        public abstract string MakeSound();
+    }
+    public class Tiger : Animal
+    {
+        public override string MakeSound()
+        {
+            return $"Tiger: roarrrrr!";
+        }
+    }
+    public class Chicken : Animal, Edible
+    {
+        public override string MakeSound()
+        {
+            return "Chicken: cluck-cluck!";
+        }
+
+        public string HowToEat()
+        {
+            return "could be fried";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Edible
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animal = new Animal[2];
+            animal[0] = new Tiger();
+            animal[1] = new Chicken();
+            foreach (Animal item in animal)
+            {
+                Console.WriteLine(item.MakeSound());
+                if (item is Chicken)
+                {
+                    Edible edible = (Chicken)item;
+                    Console.WriteLine(edible.HowToEat());
+                }
+            }
+            Fruit[] fruits = new Fruit[2];
+            fruits[0] = new Orange();
+            fruits[1] = new Apple();
+            foreach (Fruit fruit in fruits)
+            {
+                Console.WriteLine(fruit.HowToEat());
+            }
+            Console.ReadLine();
+        }
+    }
+}
